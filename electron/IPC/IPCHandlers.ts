b043945a77938b570be438ad6aaadcb25ebca932
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-04 14:01:34 +0000

    removed handleGettingData function

--- electron/IPC/IPCHandlers.ts
+++ electron/IPC/IPCHandlers.ts
@@ -4,8 +4,6 @@ import { getAllData } from "../models/databaseFunctions";
 
 const { SET_WINDOW_TITLE } = IPC_ACTIONS.Window;
 
-const { GET_ALL_DATA } = IPC_ACTIONS.Database;
-
 const handleSetWindowTitle = (event: IpcMainEvent, title: string) => {
   const webContents = event?.sender;
   const window = BrowserWindow.fromWebContents(webContents);
@@ -13,21 +11,11 @@ const handleSetWindowTitle = (event: IpcMainEvent, title: string) => {
   window.setTitle(title);
 };
 
-const handleGettingData = (event: IpcMainEvent) => {
-  const data = getAllData();
-  console.log("handleGetting data ran and the result is:", data);
-  event.reply(data);
-};
-
 const ipcHandlers = [
   {
     event: SET_WINDOW_TITLE,
     callback: handleSetWindowTitle,
-  },
-  {
-    event: GET_ALL_DATA,
-    callback: handleGettingData,
-  },
+  }
 ];
 
 // function that exports all the IPCActions
