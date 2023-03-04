873718dd6ff4232e438566c0742f1c67b467cdeb
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-04 17:55:04 +0000

    removed setWindowTitle function

--- electron/IPC/IPCHandlers.ts
+++ electron/IPC/IPCHandlers.ts
@@ -1,28 +1,9 @@
-import { BrowserWindow, ipcMain, IpcMainEvent } from "electron";
-import { IPC_ACTIONS } from "./IPCActions";
+import { ipcMain } from "electron";
 import { getAllData } from "../models/databaseFunctions";
 
-const { SET_WINDOW_TITLE } = IPC_ACTIONS.Window;
-
-const handleSetWindowTitle = (event: IpcMainEvent, title: string) => {
-  const webContents = event?.sender;
-  const window = BrowserWindow.fromWebContents(webContents);
-
-  window.setTitle(title);
-};
-
-const ipcHandlers = [
-  {
-    event: SET_WINDOW_TITLE,
-    callback: handleSetWindowTitle,
-  }
-];
 
 // function that exports all the IPCActions
 export const registerIPCHandlers = () => {
-  ipcHandlers.forEach((handler: { event: string; callback: any }) => {
-    ipcMain.on(handler.event, handler.callback);
-  });
   ipcMain.handle("get-version", async (event, args) => {
     return 1;
   });
