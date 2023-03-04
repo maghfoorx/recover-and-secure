f13bccfac6cd2466a4748d614c3e0606e506c376
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-04 18:13:29 +0000

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import HomePage from './components/HomePage'
+import { useState } from "react";
+import HomePage from "./components/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <HomePage />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
