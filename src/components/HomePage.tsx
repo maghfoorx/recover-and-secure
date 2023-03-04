9f333ada1a33e561679ed3c5d4ff85f5387eeb89
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-04 14:03:30 +0000

    added a button to get dad and version

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,10 +1,24 @@
-import { setWindowTitle } from "@/data/IPC/IPCMessages"
+import { getAllData, getVersion } from "@/data/IPC/IPCMessages";
+import { useState } from "react";
 
 export default function HomePage(): JSX.Element {
+    const [data, setData] = useState([]);
+
+    async function handleGettingData() {
+        const result = await getAllData();
+        console.log("The data received is:", result)
+    }
+
+    async function handleGetVersion() {
+        const version = await getVersion();
+        console.log(version)
+    }
+
     return (
         <>
             <h1>This is the Home Page!</h1>
-            <button onClick={() => { setWindowTitle("MKA Lost Property") }}>Check window title</button>
+            <button onClick={handleGettingData}>FetchData</button>
+            <button onClick={handleGetVersion}>Get Version</button>
         </>
-    )
+    );
 }
