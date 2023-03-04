552a009cedd6e8e70c495e6f2a52cee651fc8b63
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-04 14:51:56 +0000

    printing data from database

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,12 +1,19 @@
 import { getAllData, getVersion } from "@/data/IPC/IPCMessages";
 import { useState } from "react";
 
+interface Person {
+    id: number;
+    first_name: string;
+    second_name:string;
+}
+
 export default function HomePage(): JSX.Element {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState<Person[]>([]);
 
     async function handleGettingData() {
         const result = await getAllData();
         console.log("The data received is:", result)
+        setData(result)
     }
 
     async function handleGetVersion() {
@@ -19,6 +26,7 @@ export default function HomePage(): JSX.Element {
             <h1>This is the Home Page!</h1>
             <button onClick={handleGettingData}>FetchData</button>
             <button onClick={handleGetVersion}>Get Version</button>
+            {data.map(person => <p key={person.id}>{person.first_name}</p>)}
         </>
     );
 }
