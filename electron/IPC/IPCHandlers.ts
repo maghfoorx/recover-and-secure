42def73c7dedea583882cd716bf0f39c6bcfbbfe
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-05 17:56:12 +0000

    getting lost items

--- electron/IPC/IPCHandlers.ts
+++ electron/IPC/IPCHandlers.ts
@@ -1,13 +1,13 @@
 import { ipcMain } from "electron";
-import { getAllData } from "../models/databaseFunctions";
+import { getLostItemsReported } from "../models/databaseFunctions";
 
 // function that exports all the IPCActions
 export const registerIPCHandlers = () => {
   ipcMain.handle("get-version", async (event, args) => {
     return 1;
   });
-  ipcMain.handle("GET_DATA", async (event, args) => {
-    const data = await getAllData();
+  ipcMain.handle("GET_LOST_ITEMS", async (event, args) => {
+    const data = await getLostItemsReported();
     return data;
   });
 };
