138ac85c93a3922b0ac4267d20d2f9d7e512fc43
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-05 17:43:48 +0000

    mock data for lost items

--- electron/models/SQLQueries.sql
+++ electron/models/SQLQueries.sql
@@ -10,3 +10,10 @@ CREATE TABLE lost_items (
   PhoneNumber TEXT,
   ItemFound BOOLEAN DEFAULT false
 );
+
+-- mock data for lost_items
+INSERT INTO lost_items (ItemName, Details, LostArea, PersonName, AimsID, PhoneNumber)
+VALUES
+  ('Wallet', 'Black leather bi-fold wallet with ID and credit cards', 'Coffee shop', 'John Smith', 12345, '555-123-4567'),
+  ('Keys', 'Silver house key, car key, and office key', 'Parking lot', 'Jane Doe', 54321, '555-987-6543'),
+  ('Phone', 'iPhone X with black case and cracked screen', 'Bus station', 'Alex Lee', 67890, '555-555-1212');
