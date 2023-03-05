5c2843e8ce212226c612f8f7604f8312ca7223e0
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-05 17:57:20 +0000

    getting lost items

--- src/components/LostProperty.tsx
+++ src/components/LostProperty.tsx
@@ -1,4 +1,18 @@
+import { getLostItemsReported } from "@/data/IPC/IPCMessages"
+import { useEffect, useState } from "react"
 export default function LostProperty(): JSX.Element {
+
+    const [lostItems, setLostItems] = useState<any>([])
+
+    useEffect(() => {
+        handleGetLostItems();
+    }, [])
+
+    async function handleGetLostItems() {
+        const response = await getLostItemsReported()
+        setLostItems(response);
+    }
+    console.log(lostItems);
     return (
         <>
             <h1>This is Lost Property Page</h1>
