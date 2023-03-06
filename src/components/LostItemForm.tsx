5f40cbb19eb682082ddc42c6070e7ae545af8fcc
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-06 16:43:53 +0000

    added function to post data to database

--- src/components/LostItemForm.tsx
+++ src/components/LostItemForm.tsx
@@ -1,13 +1,29 @@
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { postLostItem } from "@/data/IPC/IPCMessages";
+import { useState } from "react";
 
 export default function LostItemForm(): JSX.Element {
+
+    const [sucess, setSuccess] = useState<boolean>(false);
+
     const { register, handleSubmit, formState: { errors }, } = useForm();
+
+    async function handlePostingForm(data: unknown) {
+        try {
+            await postLostItem(data)
+            setSuccess(true);
+            setTimeout(() => setSuccess(false), 2000)
+        }
+        catch (error) {
+            console.error(error)
+        }
+    }
     return (
         <>
             <Link to="/">Go Back</Link>
             <h1>This is the form to submit lost property!</h1>
-            <form onSubmit={handleSubmit((data) => console.log("This is data the form is submitting:", data))}>
+            <form onSubmit={handleSubmit((data) => handlePostingForm(data))}>
                 <p>Person Name</p>
                 <input {...register("PersonName")} />
                 <p>Item Name</p>
@@ -17,11 +33,12 @@ export default function LostItemForm(): JSX.Element {
                 <p>Lost Area</p>
                 <input {...register("LostArea")} />
                 <p>Aims ID</p>
-                <input type="number" {...register("Aims ID")} />
+                <input type="number" {...register("AimsID")} />
                 <p>Phone Number</p>
                 <input {...register("PhoneNumber")} />
                 <br />
                 <input type="submit" />
+                {sucess && <h3>Successfully Posted the form!</h3>}
             </form>
         </>
     )
