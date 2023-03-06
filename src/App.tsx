de4f8451c0b057101d20990032e3fead484393b4
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-06 14:26:05 +0000

    added route for lost item form

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar";
 import { Routes, Route } from "react-router-dom"
 import LostProperty from "./components/LostProperty";
 import Amaanat from "./components/Amaanat";
+import LostItemForm from "./components/LostItemForm";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LostProperty />} />
         <Route path="/amaanat" element={<Amaanat />} />
+        <Route path="/lost-item-form" element={<LostItemForm />} />
       </Routes>
     </div>
   );
