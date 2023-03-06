a1b8c8c28d66756662f70a9688dd67bbec8e907b
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-06 11:25:43 +0000

    setup a grid of lost property items

--- src/components/LostProperty.tsx
+++ src/components/LostProperty.tsx
@@ -1,21 +1,37 @@
+import { LostItemType } from "@/data/Interfaces";
 import { getLostItemsReported } from "@/data/IPC/IPCMessages"
 import { useEffect, useState } from "react"
+import DataGrid from "react-data-grid";
+import 'react-data-grid/lib/styles.css';
+
 export default function LostProperty(): JSX.Element {
 
-    const [lostItems, setLostItems] = useState<any>([])
+    const [lostItems, setLostItems] = useState<LostItemType[]>([])
 
     useEffect(() => {
         handleGetLostItems();
-    }, [])
+    }, []);
 
     async function handleGetLostItems() {
         const response = await getLostItemsReported()
         setLostItems(response);
     }
     console.log(lostItems);
+
+    //creating columns
+    const columns = [
+        { key: "ID", name: "ID", width: 10 },
+        { key: "ItemName", name: "Name", width: 20 },
+        { key: "Details", name: "Details" },
+        { key: "LostArea", name: "Lost Area", width: 100 },
+        { key: "Date", name: "Date Lost", width: 90 },
+        { key: "ItemFound", name: "Found", width: 90 }
+    ]
+
     return (
         <>
             <h1>This is Lost Property Page</h1>
+            <DataGrid columns={columns} rows={lostItems} rowHeight={40} />
         </>
     )
 }
