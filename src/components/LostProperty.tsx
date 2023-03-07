23ddaf3ac329c2542b6257190f9eaf6ead2771ed
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-07 22:13:59 +0000

    added materialUI table

--- src/components/LostProperty.tsx
+++ src/components/LostProperty.tsx
@@ -1,10 +1,13 @@
 import useFetchLostItems from "@/customHooks/useFetchLostItems";
 import { LostItemType } from "@/data/Interfaces";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DataGrid from "react-data-grid";
 import 'react-data-grid/lib/styles.css';
 import { Link } from "react-router-dom";
 import PopUp from "./PopUp";
+import TableLostItems from "./TableLostItems";
+import MaterialTable from "material-table";
+import { ThemeProvider, createTheme } from "@mui/material";
 
 export default function LostProperty(): JSX.Element {
 
@@ -13,34 +16,83 @@ export default function LostProperty(): JSX.Element {
     const [popup, setPopup] = useState<boolean>(false);
     const [PopUpData, setPopUpData] = useState<null | LostItemType>(null)
 
-    const { lostItems } = useFetchLostItems();
+    const { lostItems, handleGetLostItems } = useFetchLostItems();
+
+    useEffect(() => {
+        handleGetLostItems()
+    }, [popup])
 
     //creating columns
-    const columns = [
-        { key: "ID", name: "ID", width: 10 },
+    // const columns = [
+    //     { key: "ID", name: "ID", width: 10 },
+    //     {
+    //         key: "ItemName", name: "Name", width: 20,
+    //         formatter: ({ row }: any) => (
+    //             <div onClick={() => {
+    //                 setPopUpData(row)
+    //                 setPopup(!popup)
+    //             }} style={{ cursor: "pointer" }}>{row.ItemName}</div>
+    //         )
+    //     },
+    //     { key: "Details", name: "Details" },
+    //     { key: "LostArea", name: "Lost Area", width: 100 },
+    //     { key: "Date", name: "Date Lost", width: 90 },
+    //     { key: "ItemFound", name: "Found", width: 90 }
+    // ]
+
+    const COLUMNS = [
         {
-            key: "ItemName", name: "Name", width: 20,
-            formatter: ({ row }: any) => (
-                <div onClick={() => {
-                    setPopUpData(row)
-                    setPopup(!popup)
-                }} style={{ cursor: "pointer" }}>{row.ItemName}</div>
-            )
+            title: "ID",
+            field: "ID"
         },
-        { key: "Details", name: "Details" },
-        { key: "LostArea", name: "Lost Area", width: 100 },
-        { key: "Date", name: "Date Lost", width: 90 },
-        { key: "ItemFound", name: "Found", width: 90 }
+        {
+            title: "Name",
+            field: "ItemName",
+            render: ((rowData: LostItemType) => (
+                <div onClick={() => {
+                    setPopUpData(rowData)
+                    setPopup(!popup)
+                }} style={{ cursor: "pointer" }}>
+                    {rowData.ItemName}
+                </div>
+            ))
+        },
+        {
+            title: "Details",
+            field: "Details"
+        },
+        {
+            title: "Lost Area",
+            field: "LostArea"
+        },
+        {
+            title: "Date",
+            field: "Date"
+        },
+        {
+            title: "Found",
+            field: "ItemFound"
+        }
     ]
+
+    const defaultMaterialTheme = createTheme()
 
     return (
         <>
             <h1>This is Lost Property Page</h1>
             <Link to="/lost-item-form">Report Lost Item</Link>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            {/* <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
                 <DataGrid columns={columns} rows={lostItems} rowKeyGetter={(row: LostItemType) => row.ID} rowHeight={45} style={{ height: "100vh", width: "90vw" }} />
-            </div>
+            </div> */}
             <PopUp popup={popup} setPopup={setPopup} item={PopUpData} />
+            <div style={{ width: '100%', height: '100%' }}>
+                <ThemeProvider theme={defaultMaterialTheme}>
+                    <MaterialTable
+                        columns={COLUMNS}
+                        data={lostItems}
+                    />
+                </ThemeProvider>
+            </div>
         </>
     )
 }
