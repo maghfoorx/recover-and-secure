a3a5737e62c0efe6a87e5dcfc6d3e1cfe8e12105
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-07 15:20:01 +0000

    Setup state for popup window and added onClick on name column

--- src/components/LostProperty.tsx
+++ src/components/LostProperty.tsx
@@ -1,19 +1,32 @@
 import useFetchLostItems from "@/customHooks/useFetchLostItems";
 import { LostItemType } from "@/data/Interfaces";
-import { getLostItemsReported } from "@/data/IPC/IPCMessages"
-import { useEffect, useState } from "react"
+import { useState } from "react";
 import DataGrid from "react-data-grid";
 import 'react-data-grid/lib/styles.css';
 import { Link } from "react-router-dom";
+import PopUp from "./PopUp";
 
 export default function LostProperty(): JSX.Element {
+
+    //Popupstate declaration
+
+    const [popup, setPopup] = useState<boolean>(false);
+    const [PopUpData, setPopUpData] = useState<null | LostItemType>(null)
 
     const { lostItems } = useFetchLostItems();
 
     //creating columns
     const columns = [
         { key: "ID", name: "ID", width: 10 },
-        { key: "ItemName", name: "Name", width: 20 },
+        {
+            key: "ItemName", name: "Name", width: 20,
+            formatter: ({ row }: any) => (
+                <div onClick={() => {
+                    setPopUpData(row)
+                    setPopup(!popup)
+                }} style={{ cursor: "pointer" }}>{row.ItemName}</div>
+            )
+        },
         { key: "Details", name: "Details" },
         { key: "LostArea", name: "Lost Area", width: 100 },
         { key: "Date", name: "Date Lost", width: 90 },
@@ -25,8 +38,9 @@ export default function LostProperty(): JSX.Element {
             <h1>This is Lost Property Page</h1>
             <Link to="/lost-item-form">Report Lost Item</Link>
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-                <DataGrid columns={columns} rows={lostItems} rowHeight={45} style={{ height: "100vh", width: "90vw" }} />
+                <DataGrid columns={columns} rows={lostItems} rowKeyGetter={(row: LostItemType) => row.ID} rowHeight={45} style={{ height: "100vh", width: "90vw" }} />
             </div>
+            <PopUp popup={popup} setPopup={setPopup} item={PopUpData} />
         </>
     )
 }
