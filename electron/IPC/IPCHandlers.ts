5354836472a4c07b5dbd74bf90280e62af0ba039
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-07 16:54:28 +0000

    DELETE_LOST_ITEM message handling

--- electron/IPC/IPCHandlers.ts
+++ electron/IPC/IPCHandlers.ts
@@ -1,5 +1,6 @@
 import { ipcMain } from "electron";
 import {
+  deleteLostItem,
   getLostItemsReported,
   postLostItem,
 } from "../models/databaseFunctions";
@@ -10,12 +11,19 @@ export const registerIPCHandlers = () => {
   ipcMain.handle("get-version", async (event, args) => {
     return 1;
   });
+
   ipcMain.handle("GET_LOST_ITEMS", async (event, args) => {
     const data = await getLostItemsReported();
     return data;
   });
+
   ipcMain.handle("POST_LOST_ITEM", async (event, args: PostLostItemType) => {
     const response = await postLostItem(args);
+    return response;
+  });
+
+  ipcMain.handle("DELETE_LOST_ITEM", async (event, args: number) => {
+    const response = await deleteLostItem(args);
     return response;
   });
 };
