773898a43bf40e54c0cf6ce9f84f70e9fa4f715a
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-12 12:48:42 +0000

    imported FoundItems component

--- src/Pages/LostPropertyPage.tsx
+++ src/Pages/LostPropertyPage.tsx
@@ -1,9 +1,11 @@
+import FoundItems from "@/components/FoundItems";
 import LostItems from "@/components/LostItems";
 
 export default function LostPropertyPage(): JSX.Element {
     return (
         <div>
             <LostItems />
+            <FoundItems />
         </div>
     )
 }
