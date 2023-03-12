ef2588f14c6c8fe3cf2a8fd5edf30eed7151ac2f
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-12 12:58:13 +0000

    created found item form page

---
+++ src/Pages/FoundItemForm.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function FoundItemForm(): JSX.Element {
+    return (
+        <div>
+            <Link to="/">Go Back</Link>
+            <h1>Wow! Item Found! fill this out</h1>
+        </div>
+    )
+}
