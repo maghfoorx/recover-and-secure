f2dd9e44fb511c0b05354383a210d77737844eb4
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-12 12:48:15 +0000

    created found items component

---
+++ src/components/FoundItems.tsx
@@ -0,0 +1,9 @@
+import "../styles/FoundItems.css"
+
+export default function FoundItems(): JSX.Element {
+    return (
+        <div className="found-items-component">
+            <h1>This is Found Items Component</h1>
+        </div>
+    )
+}
