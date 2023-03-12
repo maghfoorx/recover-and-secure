366cabe9895824e4539073a169b793ea2306e8a2
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-03-12 12:58:38 +0000

    created lostItem form page

--- src/Pages/LostPropertyPage.tsx
+++ src/Pages/LostPropertyPage.tsx
@@ -1,9 +1,13 @@
 import FoundItems from "@/components/FoundItems";
 import LostItems from "@/components/LostItems";
+import { Link } from "react-router-dom";
 
 export default function LostPropertyPage(): JSX.Element {
     return (
         <div>
+            <Link to="/lost-item-form">Report Lost Item</Link>
+            <br />
+            <Link to="/found-item-form">Submit Found Item</Link>
             <LostItems />
             <FoundItems />
         </div>
