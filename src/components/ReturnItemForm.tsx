062fa415e198c3e18808de92eb550fa734dbf5dc
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-04-29 11:22:30 +0100

    created form to return a found item

---
+++ src/components/ReturnItemForm.tsx
@@ -0,0 +1,27 @@
+import {useForm} from 'react-hook-form'
+import { useState } from "react";
+import { postFoundItem, returnFoundItem } from "@/data/IPC/IPCMessages";
+
+type ReturnItemFormProperties = {
+    ItemID: number
+}
+
+export default function ReturnItemForm({ItemID}: ReturnItemFormProperties): JSX.Element {
+
+    const {register, handleSubmit, formState: {errors}, reset} = useForm();
+
+    console.log('The ID of item you are about to return is', ItemID)
+
+    return (
+        <form onSubmit={handleSubmit((data) => returnFoundItem({...data, itemID: ItemID}))}>
+            <p>Person Name</p>
+            <input {...register("PersonName", {required: true})}/>
+            <p>AIMS number</p>
+            <input {...register("AimsNumber", {required: true})}/>
+            <p>Returned By</p>
+            <input {...register("ReturnedBy", {required: true})}/>
+            <br />
+            <input type="submit"/>
+        </form>
+    )
+}
