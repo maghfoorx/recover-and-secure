9edd81531f7db315f146a9bfb868a78ef368289b
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-05-24 18:59:56 +0100

    added queries to create amaanat_items and an example insert

--- electron/database-functions/SQLQueries.amaanat.sql
+++ electron/database-functions/SQLQueries.amaanat.sql
@@ -4,3 +4,17 @@ CREATE TABLE amaanat_users (
   AIMSNo TEXT,
   PhoneNo TEXT
 );
+
+CREATE TABLE amaanat_items (
+  ID INTEGER PRIMARY KEY AUTOINCREMENT,
+  UserID INTEGER,
+  ItemName TEXT,
+  ItemDetails TEXT,
+  EntryDate DATE DEFAULT (DATE('now')),
+  ReturnedBy TEXT DEFAULT NULL,
+  Returned BOOLEAN DEFAULT 0,
+  FOREIGN KEY (UserID) REFERENCES amaanat_users(ID)
+);
+
+INSERT INTO amaanat_items (UserID, ItemName, ItemDetails, ReturnedBy)
+VALUES (1, 'Example Item', 'This is an example item');
