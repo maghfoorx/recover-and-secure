9c236475a7fc236a1979f21afe3d99b18000e038
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-06-12 14:45:36 +0100

    added printer function, no props passed yet

--- electron/printing-functions/printer.ts
+++ electron/printing-functions/printer.ts
@@ -1,4 +1,6 @@
-import { PosPrinter } from "electron-pos-printer"
+import { PosPrinter } from "electron-pos-printer";
+import * as path from "path";
+
 export async function printReceipt() {
     const options = {
         preview: true,               //  width of content body
@@ -6,16 +8,41 @@ export async function printReceipt() {
         copies: 1,                    // Number of copies to print
         printerName: 'XP-80C',        // printerName: string, check with webContent.getPrinters()
         timeOutPerLine: 1000,
-        pageSize: '80mm'  // page size
+        pageSize: '100mm'  // page size
     }
 
     const data = [
         {
-            type: 'qrCode',
-            value: 'https://github.com/Hubertformin/electron-pos-printer',
-            height: 55,
-            width: 55,
-            position: 'right'
+            type: 'image',
+            path: path.join(__dirname, '/receipt-logo.jpg'),     // file path
+            position: 'center',                                  // position of image: 'left' | 'center' | 'right'
+            width: '50px',                                           // width of image in px; default: auto
+            height: '50px',                                          // width of image in px; default: 50 or '50px'
+        },
+        {
+            type: "text",
+            value: "JALSA SALANA UK",
+            style: { fontSize: "20px", fontWeight: "bold" }
+        },
+        {
+            type: "text",
+            value: "Amanat Department",
+            style: { fontSize: "20px" }
+        },
+        {
+            type: "text",
+            value: formatDate(new Date()),
+            style: { fontSize: "20px" }
+        },
+        {
+            type: "text",
+            value: "Aims: 12345", //aims number for the future
+            style: { fontSize: "20px" }
+        },
+        {
+            type: "text",
+            value: "3 items stored", //total item number in the future
+            style: { fontSize: "20px" }
         }
     ]
 
@@ -29,4 +56,16 @@ export async function printReceipt() {
         console.log(PosPrinter)
         console.log(e);
     }
+}
+
+function formatDate(inputDate: Date) {
+    const date = new Date(inputDate);
+    const options: any = { 
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+    }
+return date.toLocaleDateString('en-GB', options);
 }
