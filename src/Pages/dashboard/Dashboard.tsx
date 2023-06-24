596adeb3e0b24ecf7c198472c5380f0c89ea9e0e
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-06-24 12:42:08 +0100

    added basic data for dashbaord

--- src/Pages/dashboard/Dashboard.tsx
+++ src/Pages/dashboard/Dashboard.tsx
@@ -1,5 +1,17 @@
+import useFetchAmaanatUsers from "@/custom-hooks/useFetchAmaanatUsers"
+import useFetchLostPropertyData from "@/custom-hooks/useFetchLostPropertyData"
+import "./dashboard.css"
+
 export default function Dashboard() {
+    const { amaanatUsers } = useFetchAmaanatUsers()
+    const { foundItems, lostItems } = useFetchLostPropertyData()
     return (
-        <h1>This is the dashboard page.</h1>
+        <div className="dashboard">
+        <h1>Lost Items Reported: {lostItems.length}</h1>
+        <h1>Reported Lost Items Found: {lostItems.filter(item => item.ItemFound === 'Yes').length}</h1>
+        <h1>Found Items: {foundItems.length}</h1>
+        <h1>Returned Items: {foundItems.filter(item => item.ReturnDate).length}</h1>
+        <h1>Amanat Users: {amaanatUsers.length}</h1>
+        </div>
     )
 }
