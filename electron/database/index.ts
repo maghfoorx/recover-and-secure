5d2d9149f12b36aac87b4a9205e1c03eab1bbc5c
Maghfoor <maghfoor.dev@gmail.com>
2025-03-19 14:15:17 +0000

    updated electorn icon and db paths

--- electron/database/index.ts
+++ electron/database/index.ts
@@ -1,9 +1,16 @@
 import knex from "knex";
+import { app } from "electron";
+import path from "path";
+
+const isDev = !app.isPackaged;
+const dbPath = isDev
+  ? "./database.sqlite3"
+  : path.join(path.dirname(process.resourcesPath), "./database.sqlite3");
 
 const db = knex({
   client: "sqlite3",
   connection: {
-    filename: "./database.sqlite3",
+    filename: dbPath,
   },
   useNullAsDefault: true,
 });
